2362fa1e5df70454441c52dfe1a7a3e86a3c12f0
redoyrahman <aanshakil@gmail.com>
2019-04-16 20:09:23 +0600

    2nd

---
+++ js/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function () {
+    $('.main-menu').meanmenu({
+        meanScreenWidth: "992",
+        meanRevealPosition: "left",
+        meanMenuContainer: '.mobile-menu-active'
+    });
+
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault()
+
+        $('html, body').animate(
+            {
+                scrollTop: $($(this).attr('href')).offset().top,
+            },
+            300,
+            'linear'
+        )
+    })
+});
